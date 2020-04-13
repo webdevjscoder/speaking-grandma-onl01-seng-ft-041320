563c3fd10df52dea66ae3ac4f82c465cdeef728f
Juan Cobian <cobianjuan19@gmail.com>
2020-04-13 22:46:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 
 def speak_to_grandma(phrase= "I LOVE YOU GRANDMA!")
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif phrase  == "Hi nane, how are you?"
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase  == "Hi!"
